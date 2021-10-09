af9f99383ff43520d1669fcc6be3c8155f6df3ec
-=Oktan=- <r.zhigalenko@mail.ru>
2021-10-10 01:58:46 +0500

    shared_ptr_toy count

--- src/main.cpp
+++ src/main.cpp
@@ -4,70 +4,73 @@
 class Toy
 {
     std::string _ltoy;
+
 public:
-    Toy(const std::string& toy = "no name"):_ltoy(toy)
-    {
-        std::cout << "constructor default\n";
-    }
-    ~Toy()
-    {
-        std::cout << "destructor\n";
-    }
-    Toy(const Toy& toy) : _ltoy(toy._ltoy)
-    {
-        std::cout << "Copy constructor worked\n";
-    }
+    Toy(const std::string &toy) : _ltoy(toy){std::cout <<"create toy"<<std::endl;}
 
-    Toy& operator= (const Toy& toy)
-    {
-        std::cout << "Operator =\n";
-        if(this == &toy)
-            return *this;
-
-        _ltoy = toy._ltoy;
-        
-        return *this;
-    }
-  
-    friend std::ostream& operator<< (std::ostream& out, Toy& toy)
-    {
-        out << toy._ltoy;
-        return out;
-    }
+    Toy() : Toy("no name") {}    
 
 };
 
 class shared_ptr_toy
 {
-    std::size_t count{0};
     Toy* _toy;
+    int* _count; //указатель на счетчик указателей
+
 public:
-    shared_ptr_toy(Toy* toy = nullptr) : _toy(toy)
+
+    shared_ptr_toy(const std::string& name)
     {
-        ++count;
-        std::cout << "Constructor chared_ptr_toy\n";
+        _toy = new Toy(name);
+        _count = new int(1); //инициализируем счетчик 1 объектом
     }
-    
-    ~shared_ptr_toy()
+    shared_ptr_toy() : shared_ptr_toy("no ptr name") {}
+
+    shared_ptr_toy(const shared_ptr_toy& oth)
     {
-        std::cout << "Destructor chared_ptr_toy\n";
-        if(count <= 1)
-        {
-            delete _toy;
-            std::cout << "destroy\n";
-        }
-        else
-        {
-            std::cout << "not destroy\n";
-        }
-            
+        _toy = new Toy(*oth._toy);
+        ++*oth._count;  //увеличиваем общий счетчик
+        _count = oth._count; //делимся указателем на счетчик
     }
 
+    shared_ptr_toy& operator= (const shared_ptr_toy& oth)
+    {
+        if(this == &oth)
+            return *this;
+        
+        if(_toy != nullptr)
+            delete _toy;
+        _toy = new Toy(*oth._toy);
+
+        return *this;
+    }
+
+    ~shared_ptr_toy()
+    {
+        std::cout <<"destruct copy toy"<<std::endl;
+        --*_count; //уменьшаем счетчик при выходи из области видимости одного из объектов
+        if(*_count == 0) //если больше нет владельцев удаляем 
+        {
+            delete _toy;
+            delete _count;
+            std::cout <<"delete toy"<<std::endl;
+        }
+
+    }
 };
+
+shared_ptr_toy make_shared_toy(const std::string& name)
+{
+    return shared_ptr_toy(name);
+}
 
 int main()
 {
-    Toy* t = new Toy("test");
-  shared_ptr_toy ptr(t);
     
+    shared_ptr_toy ptr = make_shared_toy("test");
+    {
+        shared_ptr_toy ptr2(ptr);
+        std::cout << "test2" << std::endl;
+    }
+    std::cout << "test" << std::endl;
 }
