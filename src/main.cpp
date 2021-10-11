11ea2fae43bef5aa813f6b7965339d9d52a6c2d2
Zhigalenko <r.zhigalenko@mail.ru>
2021-10-11 17:37:59 +0500

    mutex add

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <mutex>
 #include <string>
 
 class Toy
@@ -14,11 +15,27 @@ public:
 
 class shared_ptr_toy
 {
-    Toy* _toy;
-    int* _count; //указатель на счетчик указателей
-
+    Toy* _toy = nullptr;
+    int* _count = nullptr; //указатель на счетчик указателей
+    std::mutex m_ptr;
+    void add_count()
+    {
+        std::lock_guard<std::mutex> lg(m_ptr);
+        if(_count != nullptr)
+            *_count += 1;
+    }
+    void subtract_count()
+    {
+        std::lock_guard<std::mutex> lg(m_ptr);
+        if(_count != nullptr)
+            *_count -= 1;
+    }
 public:
-
+    int use_count()
+    {
+        std::lock_guard<std::mutex> lg(m_ptr);
+        return *_count;
+    }
     shared_ptr_toy(const std::string& name)
     {
         _toy = new Toy(name);
@@ -29,8 +46,8 @@ public:
     shared_ptr_toy(const shared_ptr_toy& oth)
     {
         _toy = new Toy(*oth._toy);
-        ++*oth._count;  //увеличиваем общий счетчик
         _count = oth._count; //делимся указателем на счетчик
+        add_count(); //увеличиваем общий счетчик
     }
 
     shared_ptr_toy& operator= (const shared_ptr_toy& oth)
@@ -39,16 +56,22 @@ public:
             return *this;
         
         if(_toy != nullptr)
+        {
             delete _toy;
+            delete _count;
+        }
+            
         _toy = new Toy(*oth._toy);
-
+        _count = oth._count;
+        add_count();
         return *this;
     }
 
     ~shared_ptr_toy()
     {
         std::cout <<"destruct copy toy"<<std::endl;
-        --*_count; //уменьшаем счетчик при выходи из области видимости одного из объектов
+      
+        subtract_count();//уменьшаем счетчик при выходи из области видимости одного из объектов
         if(*_count == 0) //если больше нет владельцев удаляем 
         {
             delete _toy;
@@ -68,9 +91,14 @@ int main()
 {
     
     shared_ptr_toy ptr = make_shared_toy("test");
+    std::cout << "Count: "<< ptr.use_count() << std::endl;
     {
+        
         shared_ptr_toy ptr2(ptr);
-        std::cout << "test2" << std::endl;
+        std::cout << "Count: "<< ptr.use_count() << std::endl;
+        shared_ptr_toy ptr3 = ptr;
+        std::cout << "Count: "<< ptr.use_count() << std::endl;
+       
     }
-    std::cout << "test" << std::endl;
+    std::cout << "Count: "<< ptr.use_count() << std::endl;
 }
